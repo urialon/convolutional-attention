4a8977cde7b7584cc3da0679b08081d38445a701
Ubuntu <urialon@uri.zb5la5atlzkuxejl0qrcer0yle.bx.internal.cloudapp.net>
2017-03-06 10:03:30 +0000

    fix

--- pretrained_train.sh
+++ pretrained_train.sh
@@ -1,2 +1,4 @@
-export OMP_NUM_THREADS=64
-python convolutional_attention/copy_conv_rec_learner.py ../codeattention/json/hadoop-common_train_methodnaming.json 10 150 ../codeattention/json/hadoop-common_test_methodnaming.json ../word2vec/models/js_cbow0_hs1_iter5_shuffled/vectors_filtered_test.txt
+python convolutional_attention/copy_conv_rec_learner.py ../codeattention/json/hadoop-common_train_methodnaming.json 10 150 ../codeattention/json/hadoop-common_test_methodnaming.json ../word2vec/js_cbow0_hs1_iter5_shuffled/vectors_filtered.txt
+#git add *.out
+#git commit -m "resutls"
+#git push https://urialon:urigithub123@github.com/urialon/convolutional-attention
