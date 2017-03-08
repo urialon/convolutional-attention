a141687a314e6bf22922c0309a29cd72a45651fb
Ubuntu <urialon@uri.zb5la5atlzkuxejl0qrcer0yle.bx.internal.cloudapp.net>
2017-03-08 06:34:28 +0000

    changing patience to 10

--- pretrained_train.sh
+++ pretrained_train.sh
@@ -1,4 +1,4 @@
-python convolutional_attention/copy_conv_rec_learner.py ../codeattention/json/hadoop-common_train_methodnaming.json 10 150 ../codeattention/json/hadoop-common_test_methodnaming.json ../word2vec/js_cbow0_hs1_iter5_shuffled/vectors_filtered.txt
-#git add *.out
-#git commit -m "resutls"
-#git push https://urialon:urigithub123@github.com/urialon/convolutional-attention
+python convolutional_attention/copy_conv_rec_learner.py ../codeattention/json/hadoop-common_train_methodnaming.json 1000 128 ../codeattention/json/hadoop-common_test_methodnaming.json ../word2vec/js_cbow0_hs1_iter5_min10_shuffled128/vectors_filtered.txt
+git add *.out
+git commit -m "results"
+git push https://urialon:urigithub123@github.com/urialon/convolutional-attention
